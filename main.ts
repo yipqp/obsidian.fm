b495bccec0a29a51458cadd52cbcacad54d59c93
Richard Yip <rdyip892@gmail.com>
2025-11-09 13:20:44 -0800

    create folder path suggester in settings

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 import { getAuthUrl, handleAuth, getCurrentlyPlayingTrack } from "api";
+import { FolderSuggest } from "FolderSuggest";
 
 interface MyPluginSettings {
-	mySetting: string;
+	spotifyLoggerFolderPath: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
+	spotifyLoggerFolderPath: "songs/",
 };
 
 export default class SpotifyLogger extends Plugin {
@@ -77,5 +78,26 @@ class SampleSettingTab extends PluginSettingTab {
 					window.open(authUrl);
 				}),
 			);
+
+		new Setting(containerEl).setName("Folder path").addSearch((search) => {
+			const saveFolderPath = async (value: string) => {
+				this.plugin.settings.spotifyLoggerFolderPath = value;
+				await this.plugin.saveSettings();
+			};
+
+			/* user can ignore folder suggestions and input nonexisting folder
+			 * keep this to trust that user will eventually create the folder
+			 * but need to check when before adding new song log if folder exists
+			 */
+
+			search
+				.setPlaceholder("Enter folder path")
+				.setValue(this.plugin.settings.spotifyLoggerFolderPath)
+				.onChange(saveFolderPath); // <-- ignores folder suggestion
+
+			const fs = new FolderSuggest(this.app, search.inputEl);
+
+			fs.onSelect(async () => await saveFolderPath(search.getValue()));
+		});
 	}
 }
